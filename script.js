d087b93fd0edf2eac4b7bfa6b3ce1b38450c2593
Alice-Rez <reznickova.alice@seznam.cz>
2021-04-04 00:38:11 +0200

    another analog clock created using pure CSS only

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-let clock = document.querySelector("svg");
-let offsetSeconds = 123;
-let offsetHours = -16;
-let offsetMinutes = -120;
+let clockSVG = document.querySelector("svg");
+const offsetSVG = {
+  seconds: 123,
+  hours: -16,
+  minutes: -120,
+};
+let clockCSS = document.querySelector(".clock-css");
+const offsetCSS = {
+  seconds: -90,
+  minutes: -90,
+  hours: -90,
+};
 
-setInterval(getTime, 1000);
+setInterval(() => {
+  getTime(clockSVG, "", offsetSVG);
+}, 1000);
 
-function getTime() {
+setInterval(() => {
+  getTime(clockCSS, "-css", offsetCSS);
+}, 1000);
+
+function getTime(element, specification, offset) {
   let date = new Date();
 
   let seconds = date.getSeconds();
   let hours = date.getHours();
   let minutes = date.getMinutes();
 
+  console.log(hours, minutes, seconds);
+
   if (hours >= 12) {
     hours -= 12;
   }
 
-  clock.style.setProperty("--seconds", offsetSeconds + 6 * seconds + "deg");
-  clock.style.setProperty("--hours", offsetHours + 30 * hours + "deg");
-  clock.style.setProperty("--minutes", offsetMinutes + 6 * minutes + "deg");
+  element.style.setProperty(
+    "--seconds" + specification,
+    offset.seconds + 6 * seconds + "deg"
+  );
+  element.style.setProperty(
+    "--hours" + specification,
+    offset.hours + 30 * hours + "deg"
+  );
+  element.style.setProperty(
+    "--minutes" + specification,
+    offset.minutes + 6 * minutes + "deg"
+  );
 }
